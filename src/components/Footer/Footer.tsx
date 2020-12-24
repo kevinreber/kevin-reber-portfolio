9dac9edfa80c91e3ab212e951618ba6544cfe4d3
kevinreber <kevinreber1@gmail.com>
2020-12-23 23:56:55 -0800

    added signature to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 // components
 import SocialIcons from '../SocialIcons/SocialIcons';
+import { SOCIALS } from '../../data';
 
 const Footer: React.FC = () => {
+	const linkedIn = SOCIALS[1]['src'];
 	return (
 		<>
 			<SocialIcons variant="footer" />
+			<span className="footer-text">
+				Designed & Built by{' '}
+				<a className="links" target="_blank" href={linkedIn}>
+					Kevin Reber
+				</a>
+			</span>
 		</>
 	);
 };
